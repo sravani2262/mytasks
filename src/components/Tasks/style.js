6fd999dc06331f0fe4316622e61ea811b908a3e6
Sravani <sravanichowdary2706@gmail.com>
2023-12-17 17:30:08 +0000

    myTasks

---
+++ src/components/Tasks/style.js
@@ -0,0 +1,40 @@
+import styled from 'styled-components'
+
+export const TagsCategory = styled.p`
+  background-color: #f3aa4e;
+  border: 0px;
+  border-radius: 20px;
+  height: 12px;
+  padding: 10px;
+  margin: 5px;
+  color: #131213;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+`
+
+export const ListItem = styled.li`
+  list-style-type: none;
+  background-color: #1a171d;
+  border: 0px;
+  border-radius: 20px;
+  height: 15px;
+  padding: 10px;
+  margin: 5px;
+  color: #131213;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+`
+export const TaskName = styled.p`
+  background-color: #f3aa4e;
+  border: 0px;
+  border-radius: 20px;
+  height: 12px;
+  padding: 10px;
+  margin: 5px;
+  color: #131213;
+  font-size: 12px;
+  font-weight: bold;
+  text-align: center;
+`
